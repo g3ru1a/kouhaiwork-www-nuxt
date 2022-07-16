354901747c25d4571d94f567dac2e18f9022c6dc
G3ru1a <egidiufarcas@maze.ws>
2022-07-16 22:00:23 +0000

    added discord page that redirects to discord invite link

---
+++ pages/discord.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Redirecting to discord.gg</h1>
+</template>
+
+<script>
+
+export default {
+ mounted: function(){
+  //alert('test');  
+  window.location.href = "https://discord.gg/yVv29fzfV9";
+ }
+}
+
+</script>
